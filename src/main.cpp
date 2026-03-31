f1d6a8c3d1dda0074c0c750196776e7af1742396
itsmeZeroG <87278620+itsmeZeroG@users.noreply.github.com>
2026-03-31 17:23:27 +0530

    Implemented Pins to gates

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,12 @@ class Grid {
 
     sf::Color gridColor{36, 36, 36};
 
-    for (float x = 0; x < windowSize.x; x += CELL_SIZE) {
+    for (auto x = 0.0f; x < windowSize.x; x += CELL_SIZE) {
       vertices.append(sf::Vertex{sf::Vector2f{x, 0}, gridColor});
       vertices.append(sf::Vertex{sf::Vector2f{x, windowSize.y}, gridColor});
     }
 
-    for (float y = 0; y < windowSize.y; y += CELL_SIZE) {
+    for (auto y = 0.0f; y < windowSize.y; y += CELL_SIZE) {
       vertices.append(sf::Vertex{sf::Vector2f{0, y}, gridColor});
       vertices.append(sf::Vertex{sf::Vector2f{windowSize.x, y}, gridColor});
     }
@@ -34,21 +34,79 @@ class Grid {
   void render(sf::RenderWindow& window) { window.draw(vertices); }
 };
 
+class Gate;
+class Pin {
+ public:
+  Gate* parentGate{nullptr};
+  bool state{false};
+
+  sf::CircleShape body{5.0f};  // TODO Try different point count
+
+  void render(sf::RenderWindow& window) {
+    body.setFillColor(sf::Color::Blue);
+
+    window.draw(body);
+  }
+};
+
 class Gate {
  public:
   sf::RectangleShape body;
-  bool inputA = false;
-  bool inputB = false;
-  bool output = false;
 
-  unsigned char opacity = 255;
+  std::vector<Pin> inputPins;
+  std::vector<Pin> outputPins;
 
-  Gate() {
-    body.setSize({Grid::CELL_SIZE * 4, Grid::CELL_SIZE * 3});
+  unsigned char opacity{255};
+
+  Gate(unsigned int inputCount = 1u, unsigned int outputCount = 1u) {
+    inputPins.reserve(inputCount);
+    outputPins.reserve(outputCount);
+
+    sf::Vector2f bodyPos = body.getPosition();
+    sf::Vector2f bodySize = body.getSize();
+
+    for (auto i = 0u; i < inputCount; i++) inputPins.emplace_back(this);
+    for (auto i = 0u; i < outputCount; i++) outputPins.emplace_back(this);
+
+    body.setSize(sf::Vector2f{Grid::CELL_SIZE * 4, Grid::CELL_SIZE * 3});
     body.setFillColor(sf::Color::Blue);
   }
 
-  virtual void update() = 0;
+  virtual void updateLogic() = 0;
+  void updatePins() {
+    sf::Vector2f bodyPos = body.getPosition();
+    sf::Vector2f bodySize = body.getSize();
+
+    for (size_t i = 0; i < inputPins.capacity(); i++) {
+      auto& pin = inputPins[i];
+
+      pin.body.setOrigin(pin.body.getGeometricCenter());
+
+      float posY = bodyPos.y;
+      float pinRegionSize = bodySize.y / inputPins.capacity();
+      posY += i * pinRegionSize;
+      posY += pinRegionSize / 2.0f;
+
+      pin.body.setPosition(sf::Vector2f{bodyPos.x, posY});
+    }
+
+    for (size_t i = 0u; i < outputPins.capacity(); i++) {
+      auto& pin{outputPins[i]};
+
+      pin.body.setOrigin(pin.body.getGeometricCenter());
+
+      float posY = bodyPos.y;
+      float pinRegionSize = bodySize.y / outputPins.capacity();
+      posY += i * pinRegionSize;
+      posY += pinRegionSize / 2.0f;
+
+      pin.body.setPosition(sf::Vector2f{bodyPos.x + bodySize.x, posY});
+    }
+  }
+  virtual void update() {
+    updatePins();
+    updateLogic();
+  };
 
   void render(sf::RenderWindow& window) {
     sf::Color bodyColor = body.getFillColor();
@@ -56,14 +114,19 @@ class Gate {
 
     body.setFillColor(bodyColor);
 
-    window.draw(sf::RectangleShape(body));
+    window.draw(body);
+    for (auto& pin : inputPins) pin.render(window);
+    for (auto& pin : outputPins) pin.render(window);
   }
 };
 
 class AndGate : public Gate {
-  void update() override {
-    output = inputA && inputB;
-    body.setFillColor(output ? sf::Color::Green : sf::Color::Red);
+ public:
+  AndGate() : Gate(2u, 1u) {}
+
+  void updateLogic() override {
+    outputPins[0].state = inputPins[0].state && inputPins[1].state;
+    body.setFillColor(outputPins[0].state ? sf::Color::Green : sf::Color::Red);
   }
 };
 
@@ -82,6 +145,18 @@ class GatesManager {
       if (g->body.getGlobalBounds().contains(pos)) {
         return g.get();
       }
+    }
+
+    return nullptr;
+  }
+
+  Pin* getPinAt(sf::Vector2f pos) {
+    for (const auto& g : gates) {
+      for (auto& pin : g->inputPins)
+        if (pin.body.getGlobalBounds().contains(pos)) return &pin;
+
+      for (auto& pin : g->outputPins)
+        if (pin.body.getGlobalBounds().contains(pos)) return &pin;
     }
 
     return nullptr;
@@ -106,8 +181,8 @@ class Simulation {
   sf::RenderWindow window;
   Grid grid;
 
-  bool rmbPressed = false;
-  Gate* selectedGate = nullptr;
+  bool rmbPressed{false};
+  Gate* selectedGate{nullptr};
   sf::Vector2f dragOffset{0.0f, 0.0f};
 
   Simulation(sf::Vector2u windowSize)
